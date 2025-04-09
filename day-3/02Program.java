a95764ec27d431dcf9213601b5933faaffce32d8
Sashesh Singh <sashesh.sss@gmail.com>
2025-04-10 00:10:26 +0530

    day 3: program classes

---
+++ day-3/02Program.java
@@ -0,0 +1,8 @@
+class Program{
+    public static void main(String args[]){
+        int num = 2;
+        int num1 = 5;
+        System.out.println(num1+num);
+        System.out.println(num);
+    }
+}
