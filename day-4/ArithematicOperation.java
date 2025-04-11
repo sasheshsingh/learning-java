8a8a1260f081ccbdb292cd6c081e6992aa7040a5
Sashesh Singh <sashesh.sss@gmail.com>
2025-04-11 07:36:32 +0530

    day 4: Boolean functions

---
+++ day-4/ArithematicOperation.java
@@ -0,0 +1,12 @@
+public class ArithematicOperation {
+    public static void main(String args[]){
+        System.out.println("System is working fine");
+        int num = 10;
+        num = num + 10;
+        num+=10;
+        num++;
+        ++num;
+        System.out.println(num--);
+        System.out.println(++num);
+    }    
+}
---
+++ day-4/Boolean.java
@@ -0,0 +1,14 @@
+public class Boolean {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 7;
+        int x = 3;
+        int y = 2;
+        boolean result =  a > b && x > y;
+        boolean result2 = a < b || x < y;
+        System.out.println("Result of AND operation is:");
+        System.out.println(result);
+        System.out.println("Result on OR operation is:");
+        System.out.println(result2);
+    }    
+}
